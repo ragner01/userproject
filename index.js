5e35e86af45742988b21a36383bb13ac251cad84
ragner01 <alimiomotola20@gmail.com>
2024-02-28 12:31:40 +0100

    connect to mongodb

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const mongoose = require(`mongoose`)
 
 
 const app = express()
+connectDB()
+
+
+const PORT =process.env.PORT ||4000
+
+mongoose.connection.once('open', () => {
+    console.log('Database connection success');
 
 app.listen(PORT,() => {
     console.log(`server is running on  http://localhost:${PORT}`);
+})
 })
